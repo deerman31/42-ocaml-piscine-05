f1559118360384ac375b4e5a8ef539c5130970c9
Yoshinari <110902237+deerman31@users.noreply.github.com>
2025-01-15 18:44:23 +0900

    これからex03

--- ex02/ex02.ml
+++ ex02/ex02.ml
@@ -1,15 +1,46 @@
+(*
+内部にintのペア型の変数pairを持つ、シグネチャを作成
+*)
 module type PAIR = sig
   val pair : int * int
 end
 
+(*
+内部にint型の変数xを持つ、シグネチャを作成
+*)
 module type VAL = sig
   val x : int
 end
 
-(* ここを修正してください!!! *)
+(* FIX ME !!! *)
+(* まず、Fanctor自体のシグネチャを定義 *)
+(* シグネチャの詳細について:
+   入力モジュール -> PAIR
+   出力モジュール -> VAL
+*)
+module type MAKEPROJECTION = functor (X : PAIR) -> VAL
 
+(* MAKEPROJECTIOをもとにし、MakeFstを作成 シグネチャ通り、引数にPAIRを取り、出力はVALのため、int型のxを持つ、それにPAIRの最初の値をfstで代入する *)
+module MakeFst : MAKEPROJECTION =
+functor
+  (X : PAIR)
+  ->
+  struct
+    let x = fst X.pair
+  end
 
+(* MAKEPROJECTIOをもとにし、MakeFstを作成 シグネチャ通り、引数にPAIRを取り、出力ははVALのため、int型のxを持つ、それにPAIRの2番目の値をsndで代入する *)
+module MakeSnd : MAKEPROJECTION =
+functor
+  (X : PAIR)
+  ->
+  struct
+    let x = snd X.pair
+  end
 
+(*
+PAIRシグネチャをもとにmodule pairを作成.
+*)
 module Pair : PAIR = struct
   let pair = (21, 42)
 end
--- test.ml
+++ test.ml
@@ -1,10 +1,7 @@
-let cat2 lst1 lst2 =
-  let rec aux acc lst =
-    match lst with [] -> acc | first :: rest -> aux (first :: acc) rest
-  in
-  aux lst2 (List.rev lst1)
-  (* aux (aux [] (List.rev lst2)) (List.rev lst1) *)
+module StringHashtbl = Hashtbl.Make (struct
+  type t = string
 
-let () =
-  let lst = cat2 [ 0; 1; 2; 3; 4; 5 ] [ 6; 7; 8; 9 ] in
-  List.iter (fun x -> print_endline (string_of_int x)) lst
+  let equal = String.equal
+  let hash = String.hash
+end)
+
